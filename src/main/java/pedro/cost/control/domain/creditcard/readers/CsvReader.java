cc2ac9021feb5d74b4f64993c5081df4d63a80da
Pedro0505 <pedrohrds05@hotmail.ccom>
2026-06-30 21:38:21 -0300

    fix: ajusting new nubank csv format

--- src/main/java/pedro/cost/control/domain/creditcard/readers/CsvReader.java
+++ src/main/java/pedro/cost/control/domain/creditcard/readers/CsvReader.java
@@ -1,5 +1,7 @@
 package pedro.cost.control.domain.creditcard.readers;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import pedro.cost.control.config.exceptions.BadRequestException;
@@ -8,27 +10,47 @@ import pedro.cost.control.domain.creditcard.emuns.ContentType;
 import pedro.cost.control.domain.creditcard.implementations.ReadFilesImplementations;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 @Component("text/csv")
 public class CsvReader implements ReadFilesImplementations {
     @Override
     public List<CostFileDiscriminationContext> read(MultipartFile file) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))) {
-            int headerPosition = 1;
+        try (
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))
+        ) {
+            CSVFormat format = CSVFormat.DEFAULT.builder()
+                    .setHeader()
+                    .setSkipHeaderRecord(true)
+                    .get();
 
-            Stream<CostFileDiscriminationContext> costFileDiscriminationStream = reader
-                    .lines()
-                    .skip(headerPosition)
-                    .map(e -> parseCsvString(e, ","));
+            List<CostFileDiscriminationContext> result = new ArrayList<>();
 
-            return costFileDiscriminationStream.toList();
-        } catch (IOException e) {
+            for (CSVRecord csvRecord : format.parse(reader)) {
+
+                CostFileDiscriminationContext dto = new CostFileDiscriminationContext();
+
+                dto.setDate(LocalDate.parse(csvRecord.get(0)));
+                dto.setRawDescription(csvRecord.get(1));
+                String amount = csvRecord.get(2)
+                        .replace("\"", "")
+                        .replace(" ", "")
+                        .replace(".", "")
+                        .replace(",", ".");
+
+                dto.setAmount(Double.parseDouble(amount));
+
+                result.add(dto);
+            }
+
+            return result;
+
+        } catch (Exception e) {
             throw new BadRequestException("Erro ao ler arquivo");
         }
     }
@@ -36,20 +58,5 @@ public class CsvReader implements ReadFilesImplementations {
     @Override
     public String getName() {
         return ContentType.CSV.get();
-    }
-
-    public CostFileDiscriminationContext parseCsvString(String line, String separator) {
-        String[] parts = line.split(separator);
-
-        LocalDate dateColumn = LocalDate.parse(parts[0].trim());
-        String descriptionColumn = parts[1].trim();
-        Double amountColumn = Double.valueOf(parts[2].trim());
-
-        CostFileDiscriminationContext dto = new CostFileDiscriminationContext();
-        dto.setDate(dateColumn);
-        dto.setRawDescription(descriptionColumn);
-        dto.setAmount(amountColumn);
-
-        return dto;
     }
 }
